5c1190e6464c31bcaf6c28bd075b97eb4ed475f4
Timmy Hanke <timmyhanke@gmail.com>
2022-09-21 14:05:24 +0200

    FoodDataBaseDone1

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const joi = require("joi");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+
 const { User } = require("../models/User");
 
 router.post("/", async (req, res) => {
